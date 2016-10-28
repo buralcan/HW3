a0b8c1e48180ecd4f87b8afdd0bfc9939b23c85f
buralcan <buralcan@princeton.edu>
2016-10-28 16:18:11 -0400

    Add files via upload

--- testFunctions.py
+++ testFunctions.py
@@ -15,6 +15,15 @@ class TestFunctions(unittest.TestCase):
         self.assertEqual(Df_x.shape, (1,1))
         self.assertAlmostEqual(Df_x, slope)
 
+    def testJacobian1(self):
+        x=2
+        dx = 1.e-6
+        ApproximateDf_x=F.ApproximateJacobian(F.Polynomial([5, 7, 11, 15]).f,x,dx)
+        AnalyticalDf_x=F.Polynomial([15, 11, 7, 5]).AnalyticalJacobian(x)
+        self.assertEqual(ApproximateDf_x.shape,  AnalyticalDf_x.shape)
+        self.assertAlmostEqual(ApproximateDf_x,AnalyticalDf_x, places=3)
+        #N.testing.assert_array_almost_equal(ApproximateDf_x,AnalyticalDf_x,decimal=3)
+
     def testApproxJacobian2(self):
         A = N.matrix("1. 2.; 3. 4.")
         def f(x):
@@ -25,14 +34,34 @@ class TestFunctions(unittest.TestCase):
         self.assertEqual(Df_x.shape, (2,2))
         N.testing.assert_array_almost_equal(Df_x, A)
 
+    def testJacobianMD(self):
+        x=N.matrix("2; 3; 5")
+        dx = 1.e-6
+        ApproximateDf_x=F.ApproximateJacobian(F.PolynomialMD().f,x,dx)
+        AnalyticalDf_x=F.PolynomialMD().AnalyticalJacobian(x)
+        self.assertEqual(ApproximateDf_x.shape,  AnalyticalDf_x.shape)
+        #self.assertAlmostEqual(ApproximateDf_x,AnalyticalDf_x, places=3)
+        N.testing.assert_array_almost_equal(ApproximateDf_x,  AnalyticalDf_x, decimal=3)
+
     def testPolynomial(self):
         # p(x) = x^2 + 2x + 3
         p = F.Polynomial([1, 2, 3])
         for x in N.linspace(-2,2,11):
             self.assertEqual(p(x), x**2 + 2*x + 3)
 
+"""   def testWrongJacobianMD(self):
+        x=N.matrix("2; 3; 5")
+        dx = 1.e-6
+        ApproximateDf_x=F.ApproximateJacobian(F.WrongPolynomialMD().f,x,dx)
+        AnalyticalDf_x=F.WrongPolynomialMD().AnalyticalJacobian(x)
+        self.assertEqual(ApproximateDf_x.shape,  AnalyticalDf_x.shape)
+        try: N.testing.assert_array_almost_equal(ApproximateDf_x,  AnalyticalDf_x, decimal=3)
+        #self.assertAlmostEqual(ApproximateDf_x,AnalyticalDf_x, places=3)
+        except RuntimeError:
+           print("wrong Jacobian")
+        else:
+           pass
+        #N.testing.assert_array_almost_equal(ApproximateDf_x,  AnalyticalDf_x, decimal=3) """
+
 if __name__ == '__main__':
     unittest.main()
-
-
-
--- testNewton.py
+++ testNewton.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-
+import functions as F
 import newton
 import unittest
 import numpy as N
@@ -10,6 +10,62 @@ class TestNewton(unittest.TestCase):
         solver = newton.Newton(f, tol=1.e-15, maxiter=2)
         x = solver.solve(2.0)
         self.assertEqual(x, -2.0)
+
+    def testPolynomial(self):
+        # p(x) = x^2 + 3x + 2
+        p = F.Polynomial([1, 3, 2])
+        solver = newton.Newton(p.f, tol=1.e-17, maxiter=125)
+        x1Appr = solver.solve(0.5)
+        self.assertAlmostEqual(x1Appr, -1)
+        x2Appr = solver.solve(-3.5)
+        self.assertAlmostEqual(x2Appr, -2)
+        solver = newton.Newton(p.f, Df=p.AnalyticalJacobian, tol=1.e-17, maxiter=125)
+        x2Analy = solver.solve(-3.5)
+        self.assertAlmostEqual(x2Analy, -2)
+
+    def testWrongJacobianPolynomialMD(self):
+        # p(x) = x^2 + 3x + 2
+        x=N.matrix("2; 3; 5")
+        xReal=N.matrix("7; 1; 1")
+        solver = newton.Newton(F.WrongPolynomialMD().f, Df=F.WrongPolynomialMD().AnalyticalJacobian)
+        try: xCalculated = solver.solve(x)
+        except Exception:
+            print "Wrong Jacobian"
+        else:
+            pass
+
+    def testPolynomial2(self):
+        f = lambda x : 3*x**3 - 3
+        solver = newton.Newton(f, tol=1.e-9, maxiter=25)
+        x = solver.solve(2.0)
+        self.assertAlmostEqual(x, N.matrix(1.0))
+
+    def testNorm(self):
+        f = lambda x : 2**(x-1)+3
+        x=3.0
+        try: x=newton.Newton(f, tol=1.e-15, maxiter=1).solve(x)
+        except Exception:
+            print "Approximated x is not within an acceptable radius of initial guess x0"
+
+    def testRadius(self):
+        p = F.Polynomial([1, 3, 2])
+        solver = newton.Newton(p.f, tol=1.e-15, norm=1 )
+        try: x = solver.solve(35)
+        except Exception:
+            print "Approximated x is not within an acceptable radius of initial guess x0"
+        solver = newton.Newton(p.f, Df=p.AnalyticalJacobian, tol=1.e-15, maxiter=25.0, norm=1 )
+        try: x = solver.solve(35)
+        except Exception:
+            print "Approximated x is not within an acceptable radius of initial guess x0"
+
+    def testOneStep(self):
+        x = 3
+        p = F.Polynomial([1, 3, 2])
+        solver=newton.Newton(p.f)
+        x1 = solver.step(x)
+        x2 = solver.step(x, fx=p.f(x))
+        self.assertEqual(x1, x2)
+
 
 if __name__ == "__main__":
     unittest.main()
